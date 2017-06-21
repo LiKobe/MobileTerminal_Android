b7d223d3fbc16007843433b5b8bccd6f2d4525d4
gaowubin <296414688@qq.com>
2017-06-21 11:09:38 +0800

    设置UI 完成

--- app/src/main/java/com/robotlinker/setting/SettingFragment.java
+++ app/src/main/java/com/robotlinker/setting/SettingFragment.java
@@ -5,17 +5,51 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.jakewharton.rxbinding2.view.RxView;
 import com.robotlinker.R;
 import com.robotlinker.base.BaseFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by gaowubin on 2017/6/14.
  */
 
 public class SettingFragment extends BaseFragment {
+
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.spinner)
+    Spinner spinner;
+    @BindView(R.id.tvIP)
+    TextView tvIP;
+    @BindView(R.id.edtIP)
+    EditText edtIP;
+    @BindView(R.id.textView3)
+    TextView textView3;
+    @BindView(R.id.edtPort)
+    EditText edtPort;
+    @BindView(R.id.btnConnect)
+    Button btnConnect;
+    @BindView(R.id.btnTest)
+    Button btnTest;
+    @BindView(R.id.tvAws)
+    TextView tvAws;
+    @BindView(R.id.btnAWSTest)
+    Button btnAWSTest;
 
     @Nullable
     @Override
@@ -28,7 +62,38 @@ public class SettingFragment extends BaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        List<String> list = new ArrayList<String>();
+        list.add("URIO");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        RxView.clicks(btnConnect)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception {
+                        // TODO: 2017/6/14  connet
+                    }
+                });
 
+        RxView.clicks(btnTest)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception {
+                        // TODO: 2017/6/21  test
+                    }
+                });
 
+        RxView.clicks(btnAWSTest)
+                .throttleFirst(2, TimeUnit.SECONDS)
+                .subscribe(new Consumer<Object>() {
+                    @Override
+                    public void accept(@NonNull Object o) throws Exception {
+                        // TODO: 2017/6/21  awstest
+                    }
+                });
     }
+
+
 }
