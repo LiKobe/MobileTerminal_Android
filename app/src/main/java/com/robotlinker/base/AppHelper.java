73f646f1366ae59f9f339aa572fc81bb91c4a33b
gaowubin <296414688@qq.com>
2017-06-23 22:24:08 +0800

    Merge branch 'master' of https://github.com/LiKobe/MobileTerminal_Android
    # Conflicts:
    #	app/src/main/java/com/robotlinker/jog/JogFragment.java
    #	app/src/main/java/com/robotlinker/robot/fragment/RobotFragment.java

--- app/src/main/java/com/robotlinker/base/AppHelper.java
+++ app/src/main/java/com/robotlinker/base/AppHelper.java
@@ -9,7 +9,6 @@ import com.amazonaws.regions.Regions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -22,8 +21,41 @@ import ros.rosbridge.ROSBridgeClient;
 public class AppHelper {
     // App settings
 
+    /**
+     * Add your pool id here
+     */
+    private static final String userPoolId = "ap-northeast-1_PXYjoxvmM";
+    /**
+     * Add you app id
+     */
+    private static final String clientId = "5bnuk4dt3c3fto97tfhtdidqm9";
+    /**
+     * App secret associated with your app id - if the App id does not have an associated App secret,
+     * set the App secret to null.
+     * e.g. clientSecret = null;
+     */
+    private static final String clientSecret = "vh0t5ggpmgdv4moeus3f3o7fsrbqd9ee4qd0qpulnl6nv844iqs";
+    /**
+     * Set Your User Pools region.
+     * e.g. if your user pools are in US East (N Virginia) then set cognitoRegion = Regions.US_EAST_1.
+     */
+    private static final Regions cognitoRegion = Regions.AP_NORTHEAST_1;
     //rosbridge 连接
     private static ROSBridgeClient currentClient;
+    //aws cognoti 连接
+    private static List<String> attributeDisplaySeq;
+    private static Map<String, String> signUpFieldsC2O;
+    private static Map<String, String> signUpFieldsO2C;
+    private static AppHelper appHelper;
+
+    // Change the next three lines of code to run this demo on your user pool
+    private static CognitoUserPool userPool;
+    private static String user;
+    // User details from the service
+    private static CognitoUserSession currSession;
+    private static CognitoUserDetails userDetails;
+    private static boolean emailVerified;
+    private static boolean emailAvailable;
 
     public static ROSBridgeClient getRosClient() {
         return currentClient;
@@ -32,85 +64,6 @@ public class AppHelper {
     public static void setRosClient(ROSBridgeClient client) {
         currentClient = client;
     }
-
-    //topic
-    private static String[] topicList;
-    public static String[] getTopicList() {
-        return topicList;
-    }
-    public static void setTopicList(String[] topicList) {
-        AppHelper.topicList = topicList;
-    }
-
-    //当前topic detail
-    private static String currentTopicDetail;
-    public static String getCurrentTopicDetail() {
-        if (currentTopicDetail == null)
-            return "";
-        return currentTopicDetail;
-    }
-    public static void setCurrentTopicDetail(String currentTopicDetail) {
-        AppHelper.currentTopicDetail = currentTopicDetail;
-    }
-
-    //topic detail Subscribe or unSubscribe
-    public void Subscribe(String detailName, boolean isSubscribe) {
-        if(!isSubscribe) {
-            if (getCurrentTopicDetail().equalsIgnoreCase(detailName)) {
-                currentClient.send("{\"op\":\"unsubscribe\",\"topic\":\"" + detailName + "\"}");
-                setCurrentTopicDetail(null);
-            }
-        } else {
-            if (getCurrentTopicDetail().equalsIgnoreCase(detailName)) {
-                return;
-            }
-            currentClient.send("{\"op\":\"subscribe\",\"topic\":\"" + detailName + "\"}");
-            setCurrentTopicDetail(detailName);
-        }
-    }
-
-
-
-    //aws cognoti 连接
-    private static List<String> attributeDisplaySeq;
-    private static Map<String, String> signUpFieldsC2O;
-    private static Map<String, String> signUpFieldsO2C;
-
-    private static AppHelper appHelper;
-    private static CognitoUserPool userPool;
-    private static String user;
-
-    // Change the next three lines of code to run this demo on your user pool
-
-    /**
-     * Add your pool id here
-     */
-    private static final String userPoolId = "ap-northeast-1_PXYjoxvmM";
-
-    /**
-     * Add you app id
-     */
-    private static final String clientId = "5bnuk4dt3c3fto97tfhtdidqm9";
-
-    /**
-     * App secret associated with your app id - if the App id does not have an associated App secret,
-     * set the App secret to null.
-     * e.g. clientSecret = null;
-     */
-    private static final String clientSecret = "vh0t5ggpmgdv4moeus3f3o7fsrbqd9ee4qd0qpulnl6nv844iqs";
-
-    /**
-     * Set Your User Pools region.
-     * e.g. if your user pools are in US East (N Virginia) then set cognitoRegion = Regions.US_EAST_1.
-     */
-    private static final Regions cognitoRegion = Regions.AP_NORTHEAST_1;
-
-    // User details from the service
-    private static CognitoUserSession currSession;
-    private static CognitoUserDetails userDetails;
-
-    private static boolean emailVerified;
-    private static boolean emailAvailable;
 
     public static void init(Context context) {
         setData();
@@ -152,19 +105,21 @@ public class AppHelper {
         return userPool;
     }
 
+    public static CognitoUserSession getCurrSession() {
+        return currSession;
+    }
+
     public static void setCurrSession(CognitoUserSession session) {
         currSession = session;
     }
-    public static  CognitoUserSession getCurrSession() {
-        return currSession;
+
+    public static CognitoUserDetails getUserDetails() {
+        return userDetails;
     }
 
     public static void setUserDetails(CognitoUserDetails details) {
         userDetails = details;
 //        refreshWithSync();
-    }
-    public static  CognitoUserDetails getUserDetails() {
-        return userDetails;
     }
 
     public static String getCurrUser() {
@@ -179,15 +134,13 @@ public class AppHelper {
         return emailVerified;
     }
 
-
-    public static boolean isEmailAvailable() {
-        return emailAvailable;
-    }
-
     public static void setEmailVerified(boolean emailVerif) {
         emailVerified = emailVerif;
     }
 
+    public static boolean isEmailAvailable() {
+        return emailAvailable;
+    }
 
     public static void setEmailAvailable(boolean emailAvail) {
         emailAvailable = emailAvail;
